0b742e1937a6b5e44054e37491df4afe973bf27e
zky2824193947 <2824193947@qq.com>
2021-04-09 19:18:31 +0800

    一

---
+++ js/index.js
@@ -0,0 +1,26 @@
+window.onhashchange = () => {
+    console.log('hash')
+    document.querySelector('#page').className = location.hash.replace('#', '')
+  }
+  
+  
+  let index = 0
+  let pageTabs = document.querySelectorAll('nav a')
+  let gapTime = 1000 
+  let lastTime = null
+  let nowTime = null
+  
+  document.querySelector('#page').onwheel = function(e) {
+    e.preventDefault()
+    nowTime = Date.now() 
+    if(!lastTime || nowTime - lastTime > gapTime) {
+      lastTime = nowTime
+      if(e.deltaY > 0 && index < pageTabs.length - 1 ) {
+        index++
+        pageTabs[index].click()
+      } else if(e.deltaY < 0 && index > 0) {
+        index--
+        pageTabs[index].click()
+      }
+    }
+  }
